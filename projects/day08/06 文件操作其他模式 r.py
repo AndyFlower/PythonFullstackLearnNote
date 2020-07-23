bbef9a9bef7fb40102abb5d14489a5188d6c9353
AndyFlower <1308445442@qq.com>
2020-07-23 23:05:12 +0800

    20200723 file

--- projects/day08/06 文件操作其他模式 r.py
+++ projects/day08/06 文件操作其他模式 r.py
@@ -1,6 +1,6 @@
 # 读并追加  # 顺序不能错误。 文件不存在会报错
 f = open('文件的写', encoding='utf-8', mode='r+')
-content = f.read()
+content = f.read() # 这里是读 将光标移动到最后
 print(content)
 f.write('人的一切痛苦，本质都是对自己无能的愤怒。')
 f.close()
--- projects/day08/07 文件操作其他方法.py
+++ projects/day08/07 文件操作其他方法.py
@@ -1,9 +1,9 @@
 # tell 获取光标的位置 单位字节。
 f = open('文件的写', encoding='utf-8')
-print(f.tell()) #0
+print(f.tell()) #0 读之前的光标是0
 content = f.read()
 print(content) #天天开心....人的一切痛苦，本质都是对自己无能的愤怒。
-print(f.tell())#76
+print(f.tell())#76 读之后的光标是76
 f.close()
 
 # seek 调整光标的位置 seek(n)光标移动到n位置,注意: 移动单位是byte,所有如果是utf-8的中文部分要是3的倍数
@@ -13,8 +13,8 @@ f.close()
 # 移动到开头:seek(0)
 
 # 移动到结尾:seek(0,2) seek的第二个参数表示的是从哪个位置进行偏移,默认是0,表示开头,1表示当前位置,2表示结尾
-f = open('文件的写', encoding='utf-8')
-f.seek(2)
+f = open('文件的写', encoding='gbk')
+f.seek(7)
 content = f.read()
 print(content)
 f.close()
